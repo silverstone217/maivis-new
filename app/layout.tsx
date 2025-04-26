df4bd77ddcff4ccc2bdf58e7cf00fb48db6a05ad
Stephane Mfuni <stephuni35@gmail.com>
2025-04-26 12:24:09 +0300

    scrool bar customed

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" suppressHydrationWarning>
-      <body className={` ${poppins.className} antialiased scroll-smooth`}>
+      <body
+        className={` ${poppins.className} antialiased scroll-smooth custom-Scrollbar`}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
